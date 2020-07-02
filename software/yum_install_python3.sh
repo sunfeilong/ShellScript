210cf7398493129c8f7289088ec5d98f3366dc34
sunfeilong <sunfeilong@eglsgame.com>
2020-07-02 11:10:24 +0800

    删除文件

--- software/yum_install_python3.sh
+++ software/yum_install_python3.sh
@@ -5,3 +5,5 @@ echo "install python3 start install dependence end ..."
 curl -O https://www.python.org/ftp/python/3.8.1/Python-3.8.1.tgz
 tar -avxf ./Python-3.8.1.tgz -C /usr/local/lib/python3.8.1
 cd /usr/local/lib/python3.8.1 && ./configure --enable-optimizations && make altinstall
+cd -
+rm -rf Python-3.8.1.tgz
