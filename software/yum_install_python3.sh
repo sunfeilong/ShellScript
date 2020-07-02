ca94ea24e27810647ce8ea0d3555779156280c9a
sunfeilong <sunfeilong@eglsgame.com>
2020-07-02 11:17:59 +0800

    删除多余

--- software/yum_install_python3.sh
+++ software/yum_install_python3.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-echo "install python3 start install dependence start ..."
-yum install -y gcc openssl-devel bzip2-devel libffi-devel
-echo "install python3 start install dependence end ..."
-curl -O https://www.python.org/ftp/python/3.8.1/Python-3.8.1.tgz
-tar -avxf ./Python-3.8.1.tgz -C /usr/local/lib/python3.8.1
-cd /usr/local/lib/python3.8.1 && ./configure --enable-optimizations && make altinstall
-cd -
-rm -rf Python-3.8.1.tgz
+echo "install python3 start"
+yum update -y && yum install -y python3
+echo "install python3 end"
